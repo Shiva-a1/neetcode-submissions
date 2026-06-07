a1172811630ef12a12c55a65b0364b3be82b1f30
Shiva-a1 <shiv2002a1a1a1a1@gmail.com>
2026-06-07 11:38:32 -0400

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 23
+print(type(variable))
+variable = 23.5
+print(type(variable))
+variable = True
+print(type(variable))
+variable = 'name'
+print(type(variable))
+variable = [1, 2, 3]
+print(type(variable))
